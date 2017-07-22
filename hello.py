d526b00fc93d279f7d1bae72a5a1ffefe7645327
irondoge <hwo1y8uyw2@gmail.com>
2017-07-23 00:41:18 +0200

    first commit

---
+++ hello.py
@@ -0,0 +1,13 @@
+__version__ = "1.0"
+
+def hello():
+	pass
+
+def hello_str():
+	pass
+
+def hello_stdout():
+	"""
+	Hello world function respecting the hello word standard
+	"""
+	print("hello, world")
